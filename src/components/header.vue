b190c3306e1c8c829607bdf5e21e33e26f627d8f
Aleksandar Rusev <a.rusev@hotmail.com>
2018-02-06 15:23:39 +0200

    remove unnecesary comments

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,6 @@
 <template>
 <nav class="navbar navbar-default">
   <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <a class="navbar-brand" href="#">MyCrypto</a>
     <ul class="nav navbar-nav">
@@ -9,8 +8,6 @@
         <router-link to="/market" tag="li" active-class="active"><a>Market</a></router-link>
     </ul>
       </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
       <ul class="nav navbar-nav navbar-right" v-if="!isSignedIn">
         <router-link to="/signup" tag="li" active-class="active" exact><a>Sign Up</a> </router-link>
         <router-link to="/signin" tag="li" active-class="active"><a>Sign In</a></router-link>
